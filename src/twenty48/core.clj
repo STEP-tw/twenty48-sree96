95c6e5fd7c15f47a2c9402e6c73bf7f344928a57
Sreenadh S <sreenads@thoughtworks.com>
2019-02-22 15:14:26 +0530

    Added funtionality to move row left

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,47 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
-  "Moves an entire grid to the right"
-  [grid]
-  grid)
+(def zeroes
+  (comp
+   (partial apply repeat)
+   reverse
+   (partial list 0)
+   (partial apply -)
+   (juxt first (comp count second))))
 
-(defn move-grid-left
+(def append-zeros-left
+  (comp
+   (partial apply concat)
+   (juxt second zeroes)))
+
+(def move-left
+  (comp
+   (partial map (partial apply +))
+   (partial mapcat (partial partition-all 2))
+   (partial partition-by identity)
+   (partial filter (comp not zero?))))
+
+(def move-left-and-append-zeros
+  (comp
+   append-zeros-left
+   (juxt count move-left)))
+
+
+(def move-grid-left
   "Moves an entire grid to the left"
-  [grid]
-  grid)
+  (partial map move-left-and-append-zeros))
 
-(defn move-grid-down
-  "Moves an entire grid down"
-  [grid]
-  grid)
+; (defn move-grid-right
+;   "Moves an entire grid to the right"
+;   [grid]
+;   grid)
 
-(defn move-grid-up
-  "Moves an entire grid up"
-  [grid]
-  grid)
+; (defn move-grid-down
+;   "Moves an entire grid down"
+;   [grid]
+;   grid)
+
+; (defn move-grid-up
+;   "Moves an entire grid up"
+;   [grid]
+;   grid)
